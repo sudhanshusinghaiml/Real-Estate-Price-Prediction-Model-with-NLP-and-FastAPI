b9eb70a0b0edfadcdffed756ebf940ab40647aa8
learn-modern-technology <mailmeatss2112@gmail.com>
2023-08-09 14:35:49 +0530

    Changed filename

---
+++ PropertyPricePrediction.py
@@ -0,0 +1,28 @@
+from pydantic import BaseModel, float, int
+
+class PropertyPricePrediction(BaseModel):
+    Property_Type: int
+    Club_House: int
+    School_University_In_Township: int
+    Hospital_In_Township: int
+    Mall_In_Township: int
+    Park_Jogging_Track: int
+    Swimming_Pool: int
+    Gym: int
+    Property_Area_in_SqFt: float
+    Price_By_SubArea: float
+    Amenities_Score: int
+    Price_By_Amenities_Score: float
+    Noun_Counts: int
+    Verb_Counts: int
+    Adjective_Counts: int
+    boasts_elegant: int
+    elegant_towers: int
+    every_day: int
+    great_community: int
+    mantra_gold: int
+    offering_bedroom: int
+    quality_specification: int
+    stories_offering: int
+    towers_stories: int
+    world_class: int    
