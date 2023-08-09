26b8be858726e378bed166cb4ef7d1c89143f842
learn-modern-technology <mailmeatss2112@gmail.com>
2023-08-09 15:12:38 +0530

    Added optional in the Base Model Class

--- PropertyPricePrediction.py
+++ PropertyPricePrediction.py
@@ -1,28 +1,29 @@
 from pydantic import BaseModel
+from typing import Optional
 
 class PropertyPricePrediction(BaseModel):
-    Property_Type: int
-    Club_House: int
-    School_University_In_Township: int
-    Hospital_In_Township: int
-    Mall_In_Township: int
-    Park_Jogging_Track: int
-    Swimming_Pool: int
-    Gym: int
-    Property_Area_in_SqFt: float
-    Price_By_SubArea: float
-    Amenities_Score: int
-    Price_By_Amenities_Score: float
-    Noun_Counts: int
-    Verb_Counts: int
-    Adjective_Counts: int
-    boasts_elegant: int
-    elegant_towers: int
-    every_day: int
-    great_community: int
-    mantra_gold: int
-    offering_bedroom: int
-    quality_specification: int
-    stories_offering: int
-    towers_stories: int
-    world_class: int    
+    Property_Type: Optional[int] = None
+    Club_House: Optional[int] = None
+    School_University_In_Township: Optional[int] = None
+    Hospital_In_Township: Optional[int] = None
+    Mall_In_Township: Optional[int] = None
+    Park_Jogging_Track: Optional[int] = None
+    Swimming_Pool: Optional[int] = None
+    Gym: Optional[int] = None
+    Property_Area_in_SqFt: Optional[float] = None
+    Price_By_SubArea: Optional[float] = None
+    Amenities_Score: Optional[int] = None
+    Price_By_Amenities_Score: Optional[float] = None
+    Noun_Counts: Optional[int] = None
+    Verb_Counts: Optional[int] = None
+    Adjective_Counts: Optional[int] = None
+    boasts_elegant: Optional[int] = None
+    elegant_towers: Optional[int] = None
+    every_day: Optional[int] = None
+    great_community: Optional[int] = None
+    mantra_gold: Optional[int] = None
+    offering_bedroom: Optional[int] = None
+    quality_specification: Optional[int] = None
+    stories_offering: Optional[int] = None
+    towers_stories: Optional[int] = None
+    world_class: Optional[int] = None    
