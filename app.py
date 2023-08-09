6d316440fb85a4e7a44f3831daecbd9fbed53482
learn-modern-technology <mailmeatss2112@gmail.com>
2023-08-09 14:24:33 +0530

    updated app.py

--- app.py
+++ app.py
@@ -21,18 +21,18 @@ def index():
 #    JSON data and return the predicted price with the confidence (http://127.0.0.1:8000/predictprice)
 @PropertyPricePredictionApp.post('/predictprice')
 def price_prediction(data: PropertyPricePrediction):
-    print(type(data))
     data = data.dict()
     print(data)
     
-    data = pd.DataFrame([data])
-    print(data.head())
+    data_df = pd.DataFrame([data])
+    print(data_df.head())
     
-    predicted_value = loaded_model.predict(data)
+    predicted_value = loaded_model.predict(data_df)
     print(str(predicted_value))
-    return str(predicted_value)
+    return {'prediction':str(predicted_value)}
 
 # 5. Run the API with uvicorn
 #    Will run on http://127.0.0.1:8000
+
 if __name__ == '__main__':
     uvicorn.run("app:PropertyPricePredictionApp",host='127.0.0.1', port=8005, reload=True, debug=True, workers=3)
