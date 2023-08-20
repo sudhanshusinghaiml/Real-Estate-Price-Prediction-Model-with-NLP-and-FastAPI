8553f029f40be713b74c35907bbb2f0b1ea49826
learn-modern-technology <mailmeatss2112@gmail.com>
2023-08-20 21:04:07 +0530

    Updated app.py to remove debug options

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ loaded_model = joblib.load(fileName)
 # 3. Index route, opens automatically on http://127.0.0.1:8000
 @PropertyPricePredictionApp.get('/')
 def index():
-    return {'message':'Hello!!! Welcome to the Property Price Prediction Model'}
+    return {'message': 'Hello!!! Welcome to the Property Price Prediction Model'}
 
 
 # 4. Expose the prediction functionality, make a prediction from the passed
@@ -24,10 +24,10 @@ def index():
 def price_prediction(data: PropertyPrice):
     data = data.dict()
     print(data)
-    
+
     data_df = pd.DataFrame([data])
     print(data_df.head())
-    
+
     predicted_value = loaded_model.predict(data_df)
     print(str(predicted_value))
     return str(predicted_value)
@@ -36,4 +36,4 @@ def price_prediction(data: PropertyPrice):
 # 5. Run the API with uvicorn
 #    Will run on http://127.0.0.1:8000
 if __name__ == '__main__':
-    uvicorn.run("app:PropertyPricePredictionApp",host='127.0.0.1', port=8005, reload=True, debug=True, workers=3)
+    uvicorn.run("app:PropertyPricePredictionApp", host='127.0.0.1', port=8005, reload=True, workers=3)
